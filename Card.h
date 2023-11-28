10500932b3c125cff54b31a7d0c37058bd50bb12
dprzhebelska <daria.sergiyovna@gmail.com>
2023-11-28 18:54:44 -0500

    add Cards classes

--- Card.h
+++ Card.h
@@ -15,6 +15,29 @@ class Card {
             out << c.name[0];
             return out;
         }
+        // friend istream & operator >> (istream &in, Card &c) {
+        //     string name;
+        //     cout << "Enter card name: ";
+        //     in >> name;
+        //     if(!name.compare("Blue")) {
+        //         c = Blue();
+        //     } else if (!name.compare("Chili")) {
+        //         c = Chili();
+        //     } else if (!name.compare("Stink")) {
+        //         c = Stink();
+        //     } else if (!name.compare("Green")) {
+        //         c = Green();
+        //     } else if (!name.compare("soy")) {
+        //         c = soy();
+        //     } else if (!name.compare("black")) {
+        //         c = black();
+        //     } else if (!name.compare("Red")) {
+        //         c = Red();
+        //     } else if (!name.compare("garden")) {
+        //         c = garden();
+        //     }
+        //     return in;
+        // }
 };
 
 class Blue : public Card {
