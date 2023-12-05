3477c50c994042454425fb6bc5d0882c5faacf84
Ayman Bennaceur <aymanbennaceur@aymans-mbp.lan>
2023-12-05 16:16:35 -0500

    Implemented player

---
+++ Player.cpp
@@ -0,0 +1,79 @@
+
+#include "Player.h"
+#include <iostream>
+using namespace std;
+
+//Constructeurs
+Player::Player(string& name){
+    playerName = name;
+
+}
+
+Player::Player(istream& in, CardFactory*){
+//Idk how to implement this
+}
+
+
+
+//obtenir le nom du joueur
+string Player::getName() const{
+    return playerName;
+}
+
+//returns the number of coins held by the player
+int Player::getNumCoins() const{
+    return coins;
+}
+
+//ajoute un nombre de pièces
+Player& Player::operator+=(int numCoins){
+    if (numCoins>0) {
+        coins += numCoins;
+    }
+    return *this;
+}
+
+//renvoie 2 ou 3, dependament du nombre de chains.
+int Player::getMaxNumChains() const{
+    return maxChains;
+}
+
+//renvoie le nombre de chaînes non nulles.
+int Player::getNumChains() const{
+    return numChains;
+}
+
+//renvoie la chaîne en position i.
+Chain_Base& Player::operator[](int i){
+    if (i < chain->size()) {
+        return chain->at(i);
+    }
+    //en cas d'erreur, simplement retourner premier element..
+    return chain->at(0);
+
+}
+
+//ajoute une troisième chaîne vide au joueur pour deux pièces
+void Player::buyThirdChain(){
+        if(coins < 2){
+            //Si le joueur n'a pas suffisamment de pièces alors une exception NotEnoughCoins est soulevée
+            throw new NotEnoughCoins;
+        } else{
+            maxChains++;
+            coins = coins - 2;
+        }
+    }
+
+//affiche la première carte de la main du joueur (avec l'argument False)
+//affiche l'ensemble de la main du joueur (avec l'argument True)
+//dans le flux correspondant ostream.
+void Player::printHand(ostream& out, bool argument){
+    //Implement later when we have hand.cpp ready
+}
+
+//this prints the first desired line "Dave 3 coins"
+//we need to print the rest (it says uses Hand so keeping as is for now until implemented)
+ostream& operator<<(ostream& out, const Player& player){
+    out << player.getName() << player.getNumCoins() << " coin"<< (player.coins > 1 ? "s" : "") << "\n";
+    return out;
+}
---
+++ Player.h
@@ -0,0 +1,58 @@
+
+
+#ifndef Player_h
+#define Player_h
+
+#include "CardFactory.h"
+#include "Chain.cpp"
+#include <string>
+#include <vector>
+#include <exception>
+//doesnt exist for now..
+#include "Hand.h"
+#include <iostream>
+using namespace std;
+
+
+
+class NotEnoughCoins: public exception{
+    virtual const char* what() const throw(){
+        return "Vous n'avez pas assez de pièces (2coins) pour acheter une troisième chaîne";
+    }
+};
+
+
+class Player {
+    int coins,
+    numChains = 0,
+    maxChains = 2;
+    vector<Chain_Base>* chain;
+    string playerName;
+    //Wont work until we make hand..
+    Hand hand;
+public:
+    //constructeur
+    Player(string&);
+    Player(istream& in, CardFactory*);
+
+    //returns the name of the player
+    string getName() const;
+    int getNumCoins() const;
+
+    Player& operator+=(int);
+
+    int getMaxNumChains() const;
+    int getNumChains() const;
+
+    Chain_Base& operator[](int i);
+
+    void buyThirdChain();
+    void printHand(ostream&, bool);
+
+    void operator<<(ostream& out);
+
+    friend ostream& operator<<(ostream& out, const Player& player);
+
+};
+
+#endif /* Player_h */
