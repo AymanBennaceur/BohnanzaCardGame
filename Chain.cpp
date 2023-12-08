9db543f93bb3e144154425341a8dfcdc548cefed
Ayman Bennaceur <aymanbennaceur@aymans-mbp.lan>
2023-12-08 01:56:49 -0500

    reworked chain

--- Chain.cpp
+++ Chain.cpp
@@ -1,79 +1,47 @@
-#ifndef chain_h
-#define chain_h
-
-#include <string>
 #include <vector>
 #include "Card.h"
-#include "CardFactory.h"
 #include <iostream>
-#include <typeinfo>
 using namespace std;
 
+
+#include "Chain.h"
+
+//Exception
 class IllegalTypeException : public invalid_argument {
 public:
     IllegalTypeException() : invalid_argument("IllegalTypeException: Runtime type does not match the template type of the chain.") {}
 };
 
-class IllegalType : public exception
-{
-    virtual const char* what() const throw()
-    {
-        return "Wrong type of card for this chain.";
+//ajoute une carte à Chain. Si le type du temps d’exécution ne correspond pas au type patron de la chaîne
+template<typename T>
+Chain<T>& Chain<T>::operator+=(Card* card) {
+    if (typeid(card) != typeid(cards.front())) {
+        throw new IllegalTypeException();
     }
-};
-
-class Chain_Base {
-protected:
-    string beanType;
-
-public:
-    Chain_Base(string bean) : beanType(bean) {};
-    virtual ~Chain_Base() = default;
-    virtual int sell() = 0;
-    string getBeanType() {
-        return beanType;
+    else {
+        cards.push_back(card);
     }
-    virtual void print(std::ostream&) const = 0;
+    return *this;
+}
 
-    friend ostream& operator<<(ostream& out, Chain_Base& chain) {
-        out << chain.getBeanType() << "\t";
-        chain.print(out);
-        return out;
-    };
-};
+//compte le nombre de cartes dans la chaîne courante et renvoie le nombre de pièces
+template<typename T>
+int Chain<T>::sell() {
+    int nbrCarte = getCardsPerCoin(cards.size());
+    return nbrCarte;
+}
 
-template<class T> class Chain : public Chain_Base {
-    std::vector<T*> chain_byType;
-
-public:
-    Chain() {}
-
-    virtual ~Chain() {
-        chain_byType.clear();
+//Ajouter un opérateur d’insertion pour afficher Chain
+template<typename T>
+ostream& operator <<(ostream& os, Chain<Card>& chain) {
+    os << (chain.getChain())[0].getName() << "   ";
+    for (int x = 0; x < int(chain.getChain().size());x++) {
+        os<<(chain.getChain())[x];
     }
+    return os;
+}
 
-    virtual Chain<T>& operator+=(T* gem) {
-        //not sure about this
-        if (typeid(*gem) != typeid(T)) {
-            throw IllegalTypeException();
-        }
 
-        chain_byType.push_back(gem);
-        return *this;
-    }
 
-    virtual int sell() override {
-        int numCards = static_cast<int>(chain_byType.size());
-        return chain_byType.at(0)->getCoinsPerCard(numCards);
-    }
 
-    virtual void print(std::ostream& o) const override {
-        o << getBeanType() << " ";
-        for (auto& c : chain_byType) {
-            c->print(o);
-            o << " ";
-        }
-    }
-};
 
-#endif /* chain_h */
